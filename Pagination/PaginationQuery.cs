17b44cfdde378236ac4b5a24ed0ebd73e5fc2010
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-02 09:33:40 -0300

    Criando classe de paginação

---
+++ Pagination/PaginationQuery.cs
@@ -0,0 +1,8 @@
+namespace ApiPagamentos.Pagination;
+
+public class PaginationQuery 
+{
+    public int Page { get; set; }
+
+    public int Size { get; set; }
+}
---
+++ Pagination/Paginator.cs
@@ -0,0 +1,16 @@
+namespace ApiPagamentos.Pagination;
+
+public class Paginator<T> : List<T> where T: class 
+{
+
+    public Paginator(List<T> items, PaginationQuery query)
+    {
+        AddRange(items.Skip((query.Page - 1) * query.Size).Take(query.Size));
+    }
+
+
+    public static List<T> GetPaginator(List<T> items, PaginationQuery query)
+    {
+       return new Paginator<T>(items, query);
+    }
+}
