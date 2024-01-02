ddcc757df5b174d840013590162d1c06644c63ba
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-02 09:36:34 -0300

    Adicionando validação no size do pagination

--- Pagination/PaginationQuery.cs
+++ Pagination/PaginationQuery.cs
@@ -2,7 +2,16 @@ namespace ApiPagamentos.Pagination;
 
 public class PaginationQuery 
 {
+
+    private const int LIMIT_SIZE = 50;
     public int Page { get; set; }
 
-    public int Size { get; set; }
+    private int _size = 10;
+
+    public int Size { 
+        get => _size;
+        set {
+            _size = value > 50 ? LIMIT_SIZE : value;
+        }
+    }
 }
